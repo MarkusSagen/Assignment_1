36331bca960ac46578df2952a4e59c4e1d1ccffc
AlexanderBergkvist <alexander.bergkvist@hotmail.com>
2018-09-13 14:30:41 +0200

    Lagt till import i c fil och funktion signaturer i header filen

--- hash_table.c
+++ hash_table.c
@@ -1,1 +1,6 @@
 //Vår hash_table.c C fil
+
+//OBS! Det står include "hash_table.c" i guiden men varför skulle vår
+//C fil importera sig själv?? Måste vara ett misstag så skrev:
+//include "hash_table.h" - Ta bort kommentar när du läst detta
+#include "hash_table.h"
